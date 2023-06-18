6a3769c3cc162a9b88bac6e2523c64fa5e123178
amoryc <116493561+amoryc1@users.noreply.github.com>
2023-06-18 15:23:13 +0100

    Added Zinc Ore and Zinc Bar
    They're impossible to obtain normally, so you have to use a mod like CheatSheet to get them

---
+++ matoosatron/Content/Place/ZincBar.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace matoosatron.Content.Items.Place
+{
+	public class ZincBar : ModItem
+	{
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Zinc Bar");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+		public override void SetDefaults() {
+			Item.width = 16;
+			Item.height = 16;
+			Item.useTime = 10;
+			Item.useAnimation = 15;
+			Item.consumable = true;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.value = Item.sellPrice(silver: 7, copper: 50);
+			Item.rare = ItemRarityID.White;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.maxStack = 999;
+
+			Item.createTile = ModContent.TileType<Tiles.ZincBarTile>();
+		}
+
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient(ModContent.ItemType<Items.Place.ZincOre>(), 4)
+                .AddTile(TileID.Furnaces)
+				.Register();
+		}
+	}
+}
